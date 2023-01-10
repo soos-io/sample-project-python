868ee2dd51dc8d860987efee3f0ed3beb09120e2
SOOS-JAlvarez <jalvarez@soos.io>
2023-01-10 12:20:30 -0300

    initialize package

---
+++ setup.py
@@ -0,0 +1,10 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+from setuptools import setup
+
+setup()
---
+++ src/sample_project/main.py
@@ -0,0 +1,2 @@
+def entry_point():
+  print('This is an example of a bad package.')
